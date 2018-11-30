212ad5a66f04e8db897567b429faf43efa5b1564
Chris Buckley <chris@cmbuckley.co.uk>
2018-11-30 13:50:39 +0000

    Proper class

--- index.php
+++ index.php
@@ -1,69 +1,94 @@
 <?php
 
-function getRequestHeaders() {
-    $headers = array();
-    foreach ($_SERVER as $key => $value) {
-        if (substr($key, 0, 5) == 'HTTP_' && $key != 'HTTP_HOST' && $key != 'HTTP_COOKIE') {
-            if ($key == 'HTTP_ACCEPT_ENCODING' && !extension_loaded('brotli')) { $value = str_replace(', br', '', $value); }
-            $headers[] = str_replace('_', '-', substr($key, 5)) . ': ' . $value;
-        }
-    }
-    return $headers;
-}
+class Fonts {
+    protected $server;
+    protected $body = '';
+    protected $encoding = '';
 
-function decode($string, $encoding) {
-    return transcode($string, $encoding, array(
-        'gzip' => 'gzdecode',
-        'br'   => 'brotli_uncompress',
-    ));
-}
-
-function encode($string, $encoding) {
-    return transcode($string, $encoding, array(
-        'gzip' => 'gzencode',
-        'br'   => 'brotli_compress',
-    ));
-}
-
-function transcode($string, $encoding, array $funcs) {
-    if (isset($funcs[$encoding])) {
-        $func = $funcs[$encoding];
-        return $func($string);
+    public function __construct($server) {
+        $this->server = $server;
     }
 
-    return $string;
-}
+    public function getRequestHeaders() {
+        $headers = array();
+        foreach ($this->server as $key => $value) {
+            if (substr($key, 0, 5) == 'HTTP_' && $key != 'HTTP_HOST' && $key != 'HTTP_COOKIE') {
+                // don't request brotli encoding if server can't handle it
+                if ($key == 'HTTP_ACCEPT_ENCODING' && !extension_loaded('brotli')) {
+                    $value = str_replace(', br', '', $value);
+                }
 
-$body = $encoding = '';
-$ch = curl_init();
-curl_setopt_array($ch, array(
-    CURLOPT_URL => 'https://fonts.googleapis.com/css?' . $_SERVER['QUERY_STRING'],
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_CONNECTTIMEOUT => 1,
-    CURLOPT_TIMEOUT => 5,
-    CURLOPT_HTTPHEADER => getRequestHeaders(),
-    CURLOPT_USERAGENT => $_SERVER['HTTP_USER_AGENT'],
-    CURLOPT_HEADER => false,
-    CURLOPT_BUFFERSIZE => 8192,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HEADERFUNCTION => function ($h, $header) use (&$encoding) {
-        if (substr($header, 0, 23) != 'Strict-Transport-Security' && substr($header, 0, 4) != 'Vary') {
-            header($header);
+                $headers[] = str_replace('_', '-', substr($key, 5)) . ': ' . $value;
+            }
+        }
+        return $headers;
+    }
+
+    public function decode($string) {
+        return $this->transcode($string, array(
+            'gzip' => 'gzdecode',
+            'br'   => 'brotli_uncompress',
+        ));
+    }
+
+    public function encode($string) {
+        return $this->transcode($string, array(
+            'gzip' => 'gzencode',
+            'br'   => 'brotli_compress',
+        ));
+    }
+
+    protected function transcode($string, array $funcs) {
+        if (isset($funcs[$this->encoding])) {
+            $func = $funcs[$this->encoding];
+            return $func($string);
         }
 
+        return $string;
+    }
+
+    protected function writeCurlHeader($ch, $header) {
+        // store the encoding for later
         if (substr($header, 0, 16) == 'Content-Encoding') {
-            $encoding = trim(substr($header, 17));
+            $this->encoding = trim(substr($header, 17));
         }
 
+        header($header);
         return strlen($header);
-    },
-    CURLOPT_WRITEFUNCTION => function ($h, $chunk) use (&$body) {
-        $body .= $chunk;
+    }
+
+    protected function writeCurlBody($ch, $chunk) {
+        $this->body .= $chunk;
         return strlen($chunk);
     }
-));
 
-curl_exec($ch);
-curl_close($ch);
+    public function request() {
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL            => 'https://fonts.googleapis.com/css?' . $this->server['QUERY_STRING'],
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CONNECTTIMEOUT => 1,
+            CURLOPT_TIMEOUT        => 5,
+            CURLOPT_HTTPHEADER     => $this->getRequestHeaders(),
+            CURLOPT_HEADER         => false,
+            CURLOPT_BUFFERSIZE     => 8192,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HEADERFUNCTION => array($this, 'writeCurlHeader'),
+            CURLOPT_WRITEFUNCTION  => array($this, 'writeCurlBody'),
+        ));
 
-echo encode(str_replace('}', "  font-display: swap;\n}", decode($body, $encoding)), $encoding);
+        curl_exec($ch);
+        curl_close($ch);
+        return $this->output();
+    }
+
+    protected function output() {
+        return $this->encode(str_replace('}', "  font-display: swap;\n}", $this->decode($this->body)));
+    }
+
+    public function __toString() {
+        return $this->request();
+    }
+}
+
+echo new Fonts($_SERVER);
